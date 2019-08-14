140183a8af06964216be46b4db12aeed3c150ca1
OZ20 <fariez29@gmail.com>
2019-08-14 23:52:58 +0800

    Updated map

--- app/src/main/java/com/example/neighbouradmin/HomeFragment.java
+++ app/src/main/java/com/example/neighbouradmin/HomeFragment.java
@@ -1,12 +1,20 @@
 package com.example.neighbouradmin;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,56 +22,53 @@ import com.example.neighbouradmin.Controller.IncidentController;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.LatLng;
 
-public class HomeFragment extends Fragment implements LocationListener {
-    MapView mapView;
-    GoogleMap googleMap;
+public class HomeFragment extends Fragment implements LocationListener, OnMapReadyCallback {
+    private static final String TAG = HomeFragment.class.getSimpleName();
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    private MapView mapView;
+    private GoogleMap googleMap;
     private Location location;
-    LocationManager locationManager;
+    private LocationManager locationManager;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.home_fragment, container, false);
         getActivity().setTitle("Home Page");
+        Log.d(TAG, "Starting map");
         mapView = rootView.findViewById(R.id.homeMap);
         mapView.onCreate(savedInstanceState);
-        mapView.onResume();
+        mapView.getMapAsync(this);
 
         locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-        try {
-            locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        }
-
-        MapsInitializer.initialize(getActivity().getApplicationContext());
-        if (location != null)
-            mapView.getMapAsync(new OnMapReadyCallback() {
-                @Override
-                public void onMapReady(GoogleMap map) {
-                    googleMap = map;
-
-                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                            new LatLng(location.getLatitude(), location.getLongitude()), 15));
-                    IncidentController incidentController = new IncidentController(getContext());
-                    incidentController.getIncidentsLocation(googleMap);
-                    try {
-                        googleMap.setMyLocationEnabled(true);
-                        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
-                    } catch (SecurityException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 
     @Override
     public void onLocationChanged(Location location) {
         this.location = location;
+        Log.d(TAG, "Latitude " + location.getLatitude());
+        moveCamera(location);
     }
 
     @Override
@@ -79,6 +84,94 @@ public class HomeFragment extends Fragment implements LocationListener {
     @Override
     public void onProviderDisabled(String s) {
 
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        Log.d(TAG, "Map found, check permission");
+        checkLocationPermission();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_LOCATION) {// If request is cancelled, the result arrays are empty.
+            Log.d(TAG, "Request permission result");
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "Permission granted");
+                initMap();
+            }
+        }
+    }
+
+    private void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Permission not granted");
+            Log.d(TAG, "Request pemission");
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                new AlertDialog.Builder(getContext())
+                        .setTitle(R.string.title_location_permission)
+                        .setMessage(R.string.text_location_permission)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(getActivity(),
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+        } else {
+            Log.d(TAG, "Permission granted, initialize map");
+            initMap();
+        }
+    }
+
+    private void initMap() {
+        try {
+            locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 100, this);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 100, this);
+            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (location != null)
+                moveCamera(location);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+
+//        MapsInitializer.initialize(getActivity().getApplicationContext());
+
+
+    }
+
+    private void moveCamera(final Location location) {
+        Log.d(TAG, "Moving map to: " + "\tlatitude:\t" + location.getLatitude() + "\tlongitude\t" + location.getLongitude());
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(location.getLatitude(), location.getLongitude()), 15));
+        IncidentController incidentController = new IncidentController(getContext());
+        incidentController.getIncidentsLocation(googleMap);
+        try {
+            googleMap.setMyLocationEnabled(true);
+            googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
     }
 
 
--- app/src/main/java/com/example/neighbouradmin/MainActivity.java
+++ app/src/main/java/com/example/neighbouradmin/MainActivity.java
@@ -1,23 +1,14 @@
 package com.example.neighbouradmin;
 
-import android.Manifest;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -25,30 +16,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
-
-import com.example.neighbouradmin.Controller.IncidentController;
-import com.example.neighbouradmin.model.Incident;
-import com.example.neighbouradmin.sqlite.IncidentDB;
 import com.google.firebase.firestore.GeoPoint;
-import com.google.firebase.messaging.FirebaseMessaging;
-
-import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, LocationListener {
-    private Toolbar toolbar;
-    GeoPoint currentLocation;
-    LocationManager locationManager;
-    SharedPreferences sharedPreferences;
-
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private GeoPoint currentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
@@ -64,17 +43,17 @@ public class MainActivity extends AppCompatActivity
         //home fragment
         navigationView.setCheckedItem(R.id.nav_home);
         FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment fragment = null;
-        try
-        {
-            fragment = HomeFragment.class.newInstance();
-        }catch (Exception e){
+        try {
+            fragmentManager.beginTransaction().replace(R.id.frameLayout, HomeFragment.class.newInstance()).commit();
+        } catch (IllegalAccessException | InstantiationException e) {
             e.printStackTrace();
         }
-        fragmentManager.beginTransaction().replace(R.id.frameLayout, fragment).commit();
     }
 
-
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 
     @Override
     public void onBackPressed() {
@@ -108,11 +87,11 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    private void ShowFragment(int itemId){
+    private void ShowFragment(int itemId) {
         //creating fragment object
         Fragment fragment = null;
 
-        switch (itemId){
+        switch (itemId) {
             case R.id.nav_home:
                 fragment = new HomeFragment();
                 break;
@@ -126,7 +105,7 @@ public class MainActivity extends AppCompatActivity
                 //logout();
                 break;
         }
-        if (fragment != null){
+        if (fragment != null) {
             FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
             fragmentTransaction.replace(R.id.frameLayout, fragment);
             fragmentTransaction.commit();
@@ -137,15 +116,14 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         ShowFragment(item.getItemId());
 
-       // if (id == R.id.nav_camera) {
-            // Handle the camera action
+        // if (id == R.id.nav_camera) {
+        // Handle the camera action
         //} else if (id == R.id.nav_gallery) {
 
         //} else if (id == R.id.nav_slideshow) {
@@ -153,33 +131,10 @@ public class MainActivity extends AppCompatActivity
 
         //} else if (id == R.id.nav_share) {
 
-       // } else if (id == R.id.nav_send) {
+        // } else if (id == R.id.nav_send) {
 
         //}
 
         return true;
     }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        currentLocation = new GeoPoint(location.getLatitude(), location.getLongitude());
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
-    }
-
-
-
 }
